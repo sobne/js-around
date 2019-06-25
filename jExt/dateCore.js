aa4e6ae95b72257e412bf21c05c78c37bd99b3bd
sobne <sobne.cn@gmail.com>
2019-06-25 15:01:33 +0800

    add unity

--- jExt/dateCore.js
+++ jExt/dateCore.js
@@ -49,24 +49,7 @@ Date.prototype.Format = function(format)
         ("00"+ o[k]).substr((""+ o[k]).length));
   return format;  
 }    
-   
-//+---------------------------------------------------   
-//| 求两个时间的天数差 日期格式为 YYYY-MM-dd    
-//+---------------------------------------------------   
-function daysBetween(DateOne,DateTwo)   
-{    
-    var OneMonth = DateOne.substring(5,DateOne.lastIndexOf ('-'));   
-    var OneDay = DateOne.substring(DateOne.length,DateOne.lastIndexOf ('-')+1);   
-    var OneYear = DateOne.substring(0,DateOne.indexOf ('-'));   
-   
-    var TwoMonth = DateTwo.substring(5,DateTwo.lastIndexOf ('-'));   
-    var TwoDay = DateTwo.substring(DateTwo.length,DateTwo.lastIndexOf ('-')+1);   
-    var TwoYear = DateTwo.substring(0,DateTwo.indexOf ('-'));   
-   
-    var cha=((Date.parse(OneMonth+'/'+OneDay+'/'+OneYear)- Date.parse(TwoMonth+'/'+TwoDay+'/'+TwoYear))/86400000);    
-    return Math.abs(cha);   
-}   
-   
+
    
 //+---------------------------------------------------   
 //| 日期计算   
@@ -119,55 +102,6 @@ Date.prototype.toString = function(showWeek)
     }   
     return str;   
 }   
-   
-//+---------------------------------------------------   
-//| 日期合法性验证   
-//| 格式为：YYYY-MM-DD或YYYY/MM/DD   
-//+---------------------------------------------------   
-function IsValidDate(DateStr)    
-{    
-    var sDate=DateStr.replace(/(^\s+|\s+$)/g,''); //去两边空格;    
-    if(sDate=='') return true;    
-    //如果格式满足YYYY-(/)MM-(/)DD或YYYY-(/)M-(/)DD或YYYY-(/)M-(/)D或YYYY-(/)MM-(/)D就替换为''    
-    //数据库中，合法日期可以是:YYYY-MM/DD(2003-3/21),数据库会自动转换为YYYY-MM-DD格式    
-    var s = sDate.replace(/[\d]{ 4,4 }[\-/]{ 1 }[\d]{ 1,2 }[\-/]{ 1 }[\d]{ 1,2 }/g,'');    
-    if (s=='') //说明格式满足YYYY-MM-DD或YYYY-M-DD或YYYY-M-D或YYYY-MM-D    
-    {    
-        var t=new Date(sDate.replace(/\-/g,'/'));    
-        var ar = sDate.split(/[-/:]/);    
-        if(ar[0] != t.getYear() || ar[1] != t.getMonth()+1 || ar[2] != t.getDate())    
-        {    
-            //alert('错误的日期格式！格式为：YYYY-MM-DD或YYYY/MM/DD。注意闰年。');    
-            return false;    
-        }    
-    }    
-    else    
-    {    
-        //alert('错误的日期格式！格式为：YYYY-MM-DD或YYYY/MM/DD。注意闰年。');    
-        return false;    
-    }    
-    return true;    
-}    
-   
-//+---------------------------------------------------   
-//| 日期时间检查   
-//| 格式为：YYYY-MM-DD HH:MM:SS   
-//+---------------------------------------------------   
-function CheckDateTime(str)   
-{    
-    var reg = /^(\d+)-(\d{ 1,2 })-(\d{ 1,2 }) (\d{ 1,2 }):(\d{ 1,2 }):(\d{ 1,2 })$/;    
-    var r = str.match(reg);    
-    if(r==null)return false;    
-    r[2]=r[2]-1;    
-    var d= new Date(r[1],r[2],r[3],r[4],r[5],r[6]);    
-    if(d.getFullYear()!=r[1])return false;    
-    if(d.getMonth()!=r[2])return false;    
-    if(d.getDate()!=r[3])return false;    
-    if(d.getHours()!=r[4])return false;    
-    if(d.getMinutes()!=r[5])return false;    
-    if(d.getSeconds()!=r[6])return false;    
-    return true;    
-}    
    
 //+---------------------------------------------------   
 //| 把日期分割成数组   
@@ -234,6 +168,76 @@ Date.prototype.WeekNumOfYear = function()
     var day = ary[2];      
     return result;   
 }   
+
+
+   
+//+---------------------------------------------------   
+//| 求两个时间的天数差 日期格式为 YYYY-MM-dd    
+//+---------------------------------------------------   
+function daysBetween(DateOne,DateTwo)   
+{    
+    var OneMonth = DateOne.substring(5,DateOne.lastIndexOf ('-'));   
+    var OneDay = DateOne.substring(DateOne.length,DateOne.lastIndexOf ('-')+1);   
+    var OneYear = DateOne.substring(0,DateOne.indexOf ('-'));   
+   
+    var TwoMonth = DateTwo.substring(5,DateTwo.lastIndexOf ('-'));   
+    var TwoDay = DateTwo.substring(DateTwo.length,DateTwo.lastIndexOf ('-')+1);   
+    var TwoYear = DateTwo.substring(0,DateTwo.indexOf ('-'));   
+   
+    var cha=((Date.parse(OneMonth+'/'+OneDay+'/'+OneYear)- Date.parse(TwoMonth+'/'+TwoDay+'/'+TwoYear))/86400000);    
+    return Math.abs(cha);   
+}   
+   
+
+//+---------------------------------------------------   
+//| 日期合法性验证   
+//| 格式为：YYYY-MM-DD或YYYY/MM/DD   
+//+---------------------------------------------------   
+function IsValidDate(DateStr)    
+{    
+    var sDate=DateStr.replace(/(^\s+|\s+$)/g,''); //去两边空格;    
+    if(sDate=='') return true;    
+    //如果格式满足YYYY-(/)MM-(/)DD或YYYY-(/)M-(/)DD或YYYY-(/)M-(/)D或YYYY-(/)MM-(/)D就替换为''    
+    //数据库中，合法日期可以是:YYYY-MM/DD(2003-3/21),数据库会自动转换为YYYY-MM-DD格式    
+    var s = sDate.replace(/[\d]{ 4,4 }[\-/]{ 1 }[\d]{ 1,2 }[\-/]{ 1 }[\d]{ 1,2 }/g,'');    
+    if (s=='') //说明格式满足YYYY-MM-DD或YYYY-M-DD或YYYY-M-D或YYYY-MM-D    
+    {    
+        var t=new Date(sDate.replace(/\-/g,'/'));    
+        var ar = sDate.split(/[-/:]/);    
+        if(ar[0] != t.getYear() || ar[1] != t.getMonth()+1 || ar[2] != t.getDate())    
+        {    
+            //alert('错误的日期格式！格式为：YYYY-MM-DD或YYYY/MM/DD。注意闰年。');    
+            return false;    
+        }    
+    }    
+    else    
+    {    
+        //alert('错误的日期格式！格式为：YYYY-MM-DD或YYYY/MM/DD。注意闰年。');    
+        return false;    
+    }    
+    return true;    
+}    
+   
+//+---------------------------------------------------   
+//| 日期时间检查   
+//| 格式为：YYYY-MM-DD HH:MM:SS   
+//+---------------------------------------------------   
+function CheckDateTime(str)   
+{    
+    var reg = /^(\d+)-(\d{ 1,2 })-(\d{ 1,2 }) (\d{ 1,2 }):(\d{ 1,2 }):(\d{ 1,2 })$/;    
+    var r = str.match(reg);    
+    if(r==null)return false;    
+    r[2]=r[2]-1;    
+    var d= new Date(r[1],r[2],r[3],r[4],r[5],r[6]);    
+    if(d.getFullYear()!=r[1])return false;    
+    if(d.getMonth()!=r[2])return false;    
+    if(d.getDate()!=r[3])return false;    
+    if(d.getHours()!=r[4])return false;    
+    if(d.getMinutes()!=r[5])return false;    
+    if(d.getSeconds()!=r[6])return false;    
+    return true;    
+}    
+   
    
 //+---------------------------------------------------   
 //| 字符串转成日期类型    
@@ -251,4 +255,11 @@ function StringToDate(DateStr)
         myDate = new Date(arys[0],--arys[1],arys[2]);   
     }   
     return myDate;   
+}
+
+//var d=parseDate("/Date(-62135596800000)/")
+function parseDate(timeSpan){
+	var timeSpan = timeSpan.replace('Date', '').replace('(', '').replace(')', '').replace(/\//g, '');
+	var d = new Date(parseInt(timeSpan));
+	return d;
 }
---
+++ jExt/unity.js
@@ -0,0 +1,15 @@
+//var id = getUrlParam("id");
+function getUrlParam(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if (r != null) return unescape(r[2]); return null;
+}
+function addScript(url){
+	document.write("<script language=javascript src="+url+"></script>");
+}
+function createScript(url){
+	var script = document.createElement('script');
+	script.setAttribute('type','text/javascript');
+	script.setAttribute('src',url);
+	document.getElementsByTagName('head')[0].appendChild(script);
+}
